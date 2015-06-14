22066f080681caf1741ad7bb93c9ae9cd86a20d8
Brexton <brextonpham@stanford.edu>
2015-06-14 19:47:52 -0400

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Brexton Pham on 6/14/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
