4077ac1d550b791164bf1851f7cc0e6225a856b6
Brexton <brextonpham@stanford.edu>
2015-06-29 12:36:28 -0400

    Created and passed automated tests for app

--- FunFactsTests/FunFactsTests.m
+++ FunFactsTests/FunFactsTests.m
@@ -8,8 +8,13 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "FactBook.h"
+#import "ColorWheel.h"
 
 @interface FunFactsTests : XCTestCase
+
+@property (nonatomic) FactBook *factBook;
+@property (nonatomic) ColorWheel *colorWheel;
 
 @end
 
@@ -18,23 +23,53 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.factBook = [[FactBook alloc] init];
+    self.colorWheel = [[ColorWheel alloc] init];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.factBook = nil;
+    self.colorWheel = nil;
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void) testFactBookNotNil {
+    XCTAssertNotNil(self.factBook, @"FactBook model was nil");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void) testFactsArrayNotNil {
+    XCTAssertNotNil(self.factBook.facts, @"Facts array of FactBook was nil");
 }
+
+- (void) testRandomFactNotNil {
+    NSString *randomFact = [self.factBook randomFact];
+    XCTAssertNotNil(randomFact, @"random fact was nil");
+}
+
+- (void) testRandomFactIsString {
+    id randomFact = [self.factBook randomFact];
+    XCTAssertTrue([randomFact isKindOfClass:[NSString class]], @"prediction is of class '%@'", [randomFact class]);
+}
+
+- (void) testColorWheelNotNil {
+    XCTAssertNotNil(self.colorWheel, @"ColorWheel model was nil");
+}
+
+- (void) testColorsArrayNotNil {
+    XCTAssertNotNil(self.colorWheel.colors, @"Colors array of ColorWheel was nil");
+}
+
+- (void) testRandomColorNotNil {
+    UIColor *color = [self.colorWheel randomColor];
+    XCTAssertNotNil(color, @"random color was nil");
+}
+
+- (void) testRandomColorIsColor {
+    id color = [self.colorWheel randomColor];
+    XCTAssertTrue([color isKindOfClass:[UIColor class]], @"coloris of class '%@'", [color class]);
+}
+
+
 
 @end
